10853df236e76c950da639cc143af99896a95d2e
kefujiwa <kefujiwa@student.42tokyo.jp>
2022-04-25 21:24:39 +0900

    feat: Initial commit

---
+++ create_rank_result.py
@@ -0,0 +1,142 @@
+import os
+import re
+import json
+import shutil
+import datetime
+import codecs
+import configparser
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Logger setting
+from logging import getLogger, FileHandler, DEBUG
+logger = getLogger(__name__)
+today = datetime.datetime.now()
+os.makedirs('./log', exist_ok=True)
+handler = FileHandler(f'log/{today.strftime("%Y-%m-%d")}_result.log', mode='a')
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+### functions ###
+def PrintSetUp(downloadsDirPath):
+    #印刷としてPDF保存する設定
+    chopt = Options()
+    appState = {
+        "recentDestinations": [
+            {
+                "id": "Save as PDF",
+                "origin": "local",
+                "account":""
+            }
+        ],
+        "selectedDestinationId": "Save as PDF",
+        "version": 2,
+        "isLandscapeEnabled": True, #印刷の向きを指定 tureで横向き、falseで縦向き。
+        "pageSize": 'A4', #用紙タイプ(A3、A4、A5、Legal、 Letter、Tabloidなど)
+        #"mediaSize": {"height_microns": 355600, "width_microns": 215900}, #紙のサイズ　（10000マイクロメートル = １cm）
+        "marginsType": 1, #余白タイプ #0:デフォルト 1:余白なし 2:最小
+        #"scalingType": 3 , #0：デフォルト 1：ページに合わせる 2：用紙に合わせる 3：カスタム
+        #"scaling": "141" ,#倍率
+        #"profile.managed_default_content_settings.images": 2,  #画像を読み込ませない
+        "isHeaderFooterEnabled": False, #ヘッダーとフッター
+        "isCssBackgroundEnabled": True, #背景のグラフィック
+        "isDuplexEnabled": False, #両面印刷 trueで両面印刷、falseで片面印刷
+        "isColorEnabled": True, #カラー印刷 trueでカラー、falseで白黒
+        #"isCollateEnabled": True #部単位で印刷
+    }
+    
+    prefs = {'printing.print_preview_sticky_settings.appState':
+            json.dumps(appState),
+            "profile.default_content_settings.popups": 1,
+            "download.default_directory":
+                os.path.abspath(downloadsDirPath),
+            "savefile.default_directory": 
+                os.path.abspath(downloadsDirPath),
+            "directory_upgrade": True
+    } #appState --> pref
+    chopt.add_experimental_option('prefs', prefs) #prefs --> chopt
+    chopt.add_argument('--kiosk-printing') #印刷ダイアログが開くと、印刷ボタンを無条件に押す。
+    return chopt
+
+def execute_pdf_download(driver, filePath, cnt):
+    try:
+        driver.execute_script('return window.print()')
+    except Exception as err:
+        if os.path.exists(filePath) or cnt > 3:
+            return
+        else:
+            cnt += 1
+            execute_pdf_download(driver, filePath, cnt)
+
+### main_script ###
+if __name__ == '__main__':
+
+    if len(sys.argv) == 1:
+        logger.debug('No parameter')
+        exit(1)
+
+    param = int(sys.argv[1])
+
+    downloadsDirPath = './pdf'
+    os.makedirs(downloadsDirPath, exist_ok=True)
+    shutil.rmtree(downloadsDirPath)
+    os.makedirs(downloadsDirPath, exist_ok=True)
+
+    thismonth = datetime.datetime(today.year, today.month, 1)
+
+    if param == 1:
+        lastmonth = thismonth + datetime.timedelta(days=-1)
+    else:
+        lastmonth = thismonth
+
+    year = lastmonth.strftime("%Y")
+    month = lastmonth.strftime("%m")
+
+    requestReportPath = os.environ['REQUEST_REPORT_PATH']
+    requestReportPath += f'{year}/{month}'
+    os.makedirs(requestReportPath, exist_ok=True)
+
+    defaultUrl = "https://google.co.jp/"
+
+    options = PrintSetUp(downloadsDirPath)
+    
+    try:
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        
+        driver.get(defaultUrl)
+
+        config = configparser.ConfigParser()
+        config.read_file(codecs.open("domainInfo.ini", "r", "utf8"))
+        domains = config.sections()
+
+        for domain in domains:
+            name = config[domain]['NAME']
+            client = config[domain]['CLIENT']
+            url = f"{os.environ['RANK_REPORT_PATH']}{year}/{month}/{domain}.html"
+            driver.get(f"file:///{url}")
+            sleep(10)
+
+            filePath = f"./pdf/順位計測結果.pdf"
+            execute_pdf_download(driver, filePath, 0)
+            sleep(3)
+
+            logger.debug(f"pdf download comlete: {name}")
+            clientPath = f'{requestReportPath}/{client}/'
+            os.makedirs(clientPath, exist_ok=True)
+            clientPath += f"{name}_順位計測結果_{year}_{month}.pdf"
+            shutil.move(filePath, clientPath)
+            logger.debug(f"Finish moving the pdf file: {clientPath}")
+
+        driver.close()
+        driver.quit()
+
+        exit(0)
+    except Exception as err:
+        logger.debug(f'download_result_report: {err}')
+        exit(1)
---
+++ download_result_report.py
@@ -0,0 +1,154 @@
+import os
+import re
+import shutil
+import datetime
+import gspread
+import openpyxl
+import codecs
+import configparser
+from time import sleep
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from fake_useragent import UserAgent
+from oauth2client.service_account import ServiceAccountCredentials
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Logger setting
+from logging import getLogger, FileHandler, DEBUG
+logger = getLogger(__name__)
+today = datetime.datetime.now()
+os.makedirs('./log', exist_ok=True)
+handler = FileHandler(f'log/{today.strftime("%Y-%m-%d")}_result.log', mode='a')
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+### functions ###
+def download_result_report(downloadsDirPath, SPREADSHEET_ID):
+    url = f"https://google.co.jp/"
+    
+    ua = UserAgent()
+    logger.debug(f'download_template: UserAgent: {ua.chrome}')
+
+    options = Options()
+    options.add_argument(f'user-agent={ua.chrome}')
+
+    prefs = {
+        "profile.default_content_settings.popups": 1,
+        "download.default_directory":
+                os.path.abspath(downloadsDirPath),
+        "directory_upgrade": True
+    }
+    options.add_experimental_option("prefs", prefs)
+    
+    try:
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        
+        driver.get(url)
+        driver.maximize_window()
+
+        driver.find_element_by_xpath('//a[@href="https://kiji-daiko.biz-samurai.com/order/123server/transport"]').click()
+        driver.implicitly_wait(10)
+        driver.find_element_by_id('login').click()
+        driver.implicitly_wait(10)
+        driver.find_element_by_id('login_id').send_keys(login)
+        driver.find_element_by_id('login_pass').send_keys(password)
+        driver.find_element_by_id('registrationForm').submit()
+        logger.debug('download_template: login')
+        driver.implicitly_wait(10)
+        
+        driver.find_element_by_xpath('//a[@href="/order/upload-order"]').click()
+        driver.implicitly_wait(10)
+        driver.find_elements_by_class_name('col-xs-6')[1].find_element_by_xpath('//a[@href="/img/keyword_template.xlsx"]').click()
+
+        return driver
+    except Exception as err:
+        logger.debug(f'Error: download_template: {err}')
+        exit(1)
+
+def modify_excel(fileName):
+    wb = openpyxl.load_workbook(fileName)
+
+    for sheet in wb:
+        if wb.index(sheet) != 0:
+            wb.remove(sheet)
+
+    wb.save(fileName)
+
+### main_script ###
+if __name__ == '__main__':
+
+    if len(sys.argv) == 1:
+        logger.debug('No parameter')
+        exit(1)
+
+    param = int(sys.argv[1])
+
+    downloadsDirPath = './excel'
+    os.makedirs(downloadsDirPath, exist_ok=True)
+    shutil.rmtree(downloadsDirPath)
+    os.makedirs(downloadsDirPath, exist_ok=True)
+
+    thismonth = datetime.datetime(today.year, today.month, 1)
+
+    if param == 1:
+        lastmonth = thismonth + datetime.timedelta(days=-1)
+    else:
+        lastmonth = thismonth
+
+    year = lastmonth.strftime("%Y")
+    month = lastmonth.strftime("%m")
+
+    requestReportPath = os.environ['REQUEST_REPORT_PATH']
+    requestReportPath += f'{year}/{month}'
+    os.makedirs(requestReportPath, exist_ok=True)
+
+    defaultUrl = "https://google.co.jp/"
+    ua = UserAgent()
+    logger.debug(f'download_result_report: UserAgent: {ua.chrome}')
+
+    options = Options()
+    options.add_argument(f'user-agent={ua.chrome}')
+
+    prefs = {
+        "profile.default_content_settings.popups": 1,
+        "download.default_directory":
+                os.path.abspath(downloadsDirPath),
+        "directory_upgrade": True
+    }
+    options.add_experimental_option("prefs", prefs)
+    
+    try:
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        
+        driver.get(defaultUrl)
+
+        config = configparser.ConfigParser()
+        config.read_file(codecs.open("clientInfo.ini", "r", "utf8"))
+        clients = config.sections()
+
+        for client in clients:
+            SPREADSHEET_ID = config[client]['SSID']
+            url = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/export?format=xlsx"
+            driver.get(url)
+            sleep(3)
+            fileName = f"{client}御中_成果表.xlsx"
+            filePath = f"./excel/{fileName}"
+            logger.debug(f"download: {filePath}")
+            modify_excel(filePath)
+            clientPath = f'{requestReportPath}/{client}/'
+            os.makedirs(clientPath, exist_ok=True)
+            clientPath += fileName
+            shutil.move(filePath, clientPath)
+
+        driver.close()
+        driver.quit()
+
+        exit(0)
+    except Exception as err:
+        logger.debug(f'download_result_report: {err}')
+        exit(1)
